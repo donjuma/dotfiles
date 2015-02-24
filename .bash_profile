3fd270de845617151146dfb18f4782bec31aa508
Omar Juma <Omar.Juma@wwu.edu>
2015-02-24 15:30:00 -0800

    Fixed git error

--- .bash_profile
+++
@@ -1,3 +0,0 @@
-source ~/.bashrc
-source ~/.profile
-source ~/.inputrc
--- extract.sh
+++ extract.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 VERSION_FILE=~/.dotversion
-INTERNAL_VERSION_FILE=~/.dotfiles/version
-PKG_PATH=~/.dotfiles/pkgs
+INTERNAL_VERSION_FILE=~/repos/dotfiles/version
+PKG_PATH=~/repos/dotfiles/pkgs
 
 get_installed_version() {
     local line
@@ -23,11 +23,12 @@ do_install() {
         done
     done
 
-    cp $INTERNAL_VERSION_FILE $VERSION_FILE
+    #cp $INTERNAL_VERSION_FILE $VERSION_FILE
 }
 
 if [ -f "$VERSION_FILE" ] && [ `get_pkg_version` -eq `get_installed_version`]; then
     echo "Dotfiles are already at the newest version!"
+    do_install
     exit 1
 else
     echo "Installing Dotfiles"
